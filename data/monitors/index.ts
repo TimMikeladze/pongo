97b26eb6fc39d34b55479ac76f45bf54cab07364
TimMikeladze <tim.mikeladze@gmail.com>
2025-12-06 23:53:52 -0800

    fix(monitors): restore monitors index.ts export format

--- data/monitors/index.ts
+++ data/monitors/index.ts
@@ -1,11 +1,9 @@
 // Auto-import all monitors
 // Add new monitors here
-
-import marketingWebsite from "./marketing-website";
 import productionApi from "./production-api";
+import marketingWebsite from "./marketing-website";
 
 export const monitors = {
   "production-api": productionApi,
-  "auth-service": authService,
   "marketing-website": marketingWebsite,
 };
