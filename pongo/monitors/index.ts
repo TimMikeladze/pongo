d94fa891fca29f20e16dca7a08023f28e1ce84f2
TimMikeladze <tim.mikeladze@gmail.com>
2025-12-07 16:51:34 -0800

    wip

--- pongo/monitors/index.ts
+++ pongo/monitors/index.ts
@@ -1,4 +1,17 @@
+import apiHealth from "./api-health";
+import authService from "./auth-service";
+import cdn from "./cdn";
+import database from "./database";
 import example from "./example";
 import github from "./github";
+import payments from "./payments";
 
-export default { example, github };
+export default {
+  apiHealth,
+  authService,
+  cdn,
+  database,
+  example,
+  github,
+  payments,
+};
